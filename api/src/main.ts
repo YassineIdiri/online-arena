b464ff089d961dbdd84059458ed2321669a67d74
YassineIdiri <pro.yassine.idiri@gmail.com>
2026-01-19 07:00:20 +0100

    Update CORS

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
+      "https://onlinearena.netlify.app",
     ],
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
